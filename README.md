36b57940c3c802143cbcc717c05ccf32a5b1b6ce
tracyhann <162396496+tracyhann@users.noreply.github.com>
2025-11-25 18:16:37 -0800

    example on s001 V2

---
+++ README.md
@@ -0,0 +1,29 @@
+This repo experimented a state analysis model using Hidden Markov Model (HMM), using the code from a public repo: https://github.com/vidaurre/glhmm/blob/main/docs/notebooks/GaussianHMM_example.ipynb
+
+`parcel.py`
+- Concatnated the sub-54095s001_ses-V2 resting state data (2 x 5 minutes fMRI scans at resting state). Each scan has 369 T, and is registered to the MNI152 space.
+- Parcellated by Schaefer2018 200 ROIs x 17 Networks atlas. To standardize the dimensions between the template and raw data, the atlas (182, 218, 182) is sampled using nearest neighbor to the same dimensions of the fMRI (108, 128, 108).
+- 37 ROIs of the Default Mode Network (DMN) are kept for analysis.
+
+
+
+<pre>
+.
+├── atlas
+│   ├── Schaefer2018_200Parcels_17Networks_order_FSLMNI152_1mm.nii.gz
+│   ├── Schaefer2018_200Parcels_17Networks_order_FSLMNI152_2mm.nii.gz
+│   └── schaefer2018_200parcels_17networks_order.csv
+├── data
+│   ├── 54095s001.zip
+│   └── sub-54095s001
+│       ├── 54095s001_V2_task-Resting1NewHB6scan_FD_INFO.mat
+│       ├── 54095s001_V2_task-Resting2NewHB6scan_FD_INFO.mat
+│       ├── filtreg_sm_dspk_sk_sub-54095s001_ses-V2_task-Resting1NewHB6scan_space-MNI152NLin2009cAsym_desc-preproc_bold.nii
+│       └── filtreg_sm_dspk_sk_sub-54095s001_ses-V2_task-Resting2NewHB6scan_space-MNI152NLin2009cAsym_desc-preproc_bold.nii
+├── GaussianHMM_example_in DMN\nsub-54095s001_ses-V2.ipynb - Colab.pdf
+├── parcel.py
+├── README.md
+└── ts
+    ├── data.npy
+    └── T_t.npy
+</pre>
---
+++ parcel.py
@@ -0,0 +1,69 @@
+import nibabel as nib
+from nilearn import image
+import numpy as np
+import pandas as pd
+from nilearn.maskers import NiftiLabelsMasker
+
+def generate_dmn_ts(bold_img, atlas_resamp_img, dmn_idxs):
+    print("resampled atlas:", atlas_resamp_img.shape)   
+    print("bold:", bold_img.shape)
+
+    masker = NiftiLabelsMasker(
+    labels_img=atlas_resamp_img,
+    standardize=True,        # optional, z-score per region
+    detrend=True             
+)
+
+    ts = masker.fit_transform(bold_img)
+    ts_parcels = ts.T   # shape (200, 369)
+    dts = ts_parcels[idxs].T # 1 subject T x feature (network nodes), 369, 37
+
+    return dts
+
+
+
+map = pd.read_csv('atlas/schaefer2018_200parcels_17networks_order.csv')
+# Default 75-98, 184-196
+dmn_map = {}
+for i, row in map.iterrows():
+    if 'Default' in row['network']:
+        dmn_map[row['order']] = row['network']
+
+idxs = [order - 1 for order in dmn_map.keys()]
+
+
+ex_bold_path = 'filtreg_sm_dspk_sk_sub-54095s001_ses-V2_task-Resting1NewHB6scan_space-MNI152NLin2009cAsym_desc-preproc_bold.nii'
+bold_img = nib.load(ex_bold_path)
+atlas_path = 'atlas/Schaefer2018_200Parcels_17Networks_order_FSLMNI152_1mm.nii.gz'
+atlas_img = nib.load(atlas_path) # 3D: 182 x 218 x 182
+# Resample atlas into BOLD grid (nearest to keep integer labels)
+atlas_resamp_img = image.resample_to_img(
+    atlas_img,
+    bold_img,
+    interpolation='nearest'
+)
+
+print('bold_img.shape', bold_img.shape)
+print('atlas_img.shape', atlas_img.shape)
+print('atlas_resamp_img.shape', atlas_resamp_img.shape)
+
+data = []
+T_t = []
+
+sub_fmri = {'sub-54095s001':['data/sub-54095s001/filtreg_sm_dspk_sk_sub-54095s001_ses-V2_task-Resting1NewHB6scan_space-MNI152NLin2009cAsym_desc-preproc_bold.nii',
+                             'data/sub-54095s001/filtreg_sm_dspk_sk_sub-54095s001_ses-V2_task-Resting2NewHB6scan_space-MNI152NLin2009cAsym_desc-preproc_bold.nii']}
+start, end = 0, 0
+
+for sub in sub_fmri.keys():
+    bold_paths = sub_fmri[sub]
+    T_t.append([start,end])
+    for bold_path in bold_paths:
+        bold_img = nib.load(bold_path)
+        dts = generate_dmn_ts(bold_img, atlas_resamp_img, idxs)
+        data.append(dts)
+        T_t[-1][1] += dts.shape[0]
+
+data = np.concat(data)
+np.save('ts/data.npy', data)
+T_t = np.array(T_t)
+np.save('ts/T_t.npy', T_t)
